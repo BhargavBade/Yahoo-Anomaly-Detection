b666c43f2a1889a4068e0cce5ec1bde706f3b63d
Bhargav Bade <bhargavbade@gmail.com>
2023-10-05 17:10:55 +0200

    config file for sinewaves dataset

--- Data/prepare_data.py
+++ Data/prepare_data.py
@@ -5,3 +5,5 @@ def prepare_data(data_config):
     loader = get_loader(data_config["dataset"])
     train_data, test_data, val_data = loader(**data_config).get_dataloader()
     return train_data, test_data, val_data
+
+rtztzjhj
